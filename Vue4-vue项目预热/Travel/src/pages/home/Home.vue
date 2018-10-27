4ab0e13cb65bb3c041a52126e759845cdae1223f
黄震 <1656471829@qq.com>
2018-10-27 14:29:11 +0800

    轮播图完成

--- Vue4-vue项目预热/Travel/src/pages/home/Home.vue
+++ Vue4-vue项目预热/Travel/src/pages/home/Home.vue
@@ -1,14 +1,17 @@
 <template>
 <div>
   <home-header></home-header>
+  <home-swiper></home-swiper>
 </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components:{
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   },
   data () {
     return {
