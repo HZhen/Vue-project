88ff055de81d8f29b3967d004e89f2c4ddd0c0b7
黄震 <1656471829@qq.com>
2018-10-30 19:57:14 +0800

    搜索布局完成

--- Vue4-vue项目预热/Travel/src/pages/city/City.vue
+++ Vue4-vue项目预热/Travel/src/pages/city/City.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+
 export default {
   name:'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   },
   data () {
     return {
---
+++ Vue4-vue项目预热/Travel/src/pages/city/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  </div>
+</template>
+<script>
+export default {
+  name:'CitySearch',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height .72rem
+    padding 0 .1rem
+    background $bgColor
+    .search-input
+      box-sizing border-box
+      width 100%
+      height .62rem
+      padding 0 .1rem
+      line-height .62rem
+      text-align center
+      border-radius .06rem
+      color #666666
+</style>
