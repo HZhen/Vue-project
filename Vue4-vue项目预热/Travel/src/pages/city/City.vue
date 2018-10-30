9f979fc76f3234d39e6459d210a22954e18c033c
黄震 <1656471829@qq.com>
2018-10-30 21:22:04 +0800

    列表布局完成

--- Vue4-vue项目预热/Travel/src/pages/city/City.vue
+++ Vue4-vue项目预热/Travel/src/pages/city/City.vue
@@ -2,17 +2,20 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
   </div>
 </template>
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
 
 export default {
   name:'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList
   },
   data () {
     return {
---
+++ Vue4-vue项目预热/Travel/src/pages/city/components/List.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="title border-topbottom">当前城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">成都</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">热门城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">成都</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">成都</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">成都</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">成都</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">B</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">C</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">D</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+
+  </div>
+</template>
+<script>
+export default {
+  name:'CityList',
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .border-topbottom
+    &:before
+      border-color #ccc
+    &:after
+      border-color #ccc
+   .border-bottom
+    &:before
+      border-color #ccc
+  .list
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    .title
+      line-height .44rem
+      background #eee
+      padding-left .2rem
+      color #666
+      font-size .26rem
+    .button-list
+      padding .1rem .6rem .1rem .1rem
+      overflow hidden 
+      .button-wrapper
+        float left
+        width 33.33%
+        .button
+          margin .1rem
+          padding .1rem 0
+          text-align center
+          border .02rem solid #ccc
+          border-radius .06rem
+    .item-list
+    .item
+      line-height .76rem
+      padding-left .2rem
+</style>
