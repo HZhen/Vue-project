cdbbe9be3337a5cb11645b72f41a1293d92d0a76
黄震 <1656471829@qq.com>
2018-12-05 22:23:29 +0800

    header animation

--- Vue4-vue项目预热/Travel/src/pages/detail/Detail.vue
+++ Vue4-vue项目预热/Travel/src/pages/detail/Detail.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Datail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.content{
+  height 50rem
+}
 </style>
 
---
+++ Vue4-vue项目预热/Travel/src/pages/detail/components/Header.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <router-link 
+      tag='div' 
+      to="/" 
+      class="header-abs"
+      v-show="showAbs"
+    > 
+      <i class="iconfont header-abs-back">&#xe624;</i>
+    </router-link>
+    <div 
+      class="header-fixed"
+      v-show="!showAbs"
+      :style="opacityStyle"
+    >
+      <router-link to='/'>
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>
+      景点详情
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DatailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll',this.handleScroll)
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if(top > 60 ) {
+        let opacity = top /140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity}
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header-abs
+    position absolute
+    left .2rem
+    top .2rem
+    width .8rem
+    height .8rem
+    line-height .8rem
+    border-radius .4rem
+    text-align center
+    background rgba(0, 0, 0, .6)
+    .header-abs-back
+      color #ffffff
+      font-size .4rem
+  .header-fixed
+    position fixed
+    top 0
+    left 0
+    right 0
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #fff
+    background $bgColor
+    font-size .32rem
+    .header-fixed-back
+      position absolute
+      top 0
+      left 0
+      width .64rem
+      text-align center
+      font-size .4rem
+      color #ffffff
+</style>
+
